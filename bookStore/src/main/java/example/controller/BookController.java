3e12415f74a54b13e9655554ad76de10bf5a62b6
tniu <tniu@infomedia.com.au>
2020-06-07 15:00:52 +1000

    init

--- bookStore/src/main/java/example/controller/BookController.java
+++ bookStore/src/main/java/example/controller/BookController.java
@@ -3,6 +3,7 @@ package example.controller;
 import example.model.Book;
 import example.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,8 @@ public class BookController {
     @Autowired
     private IBookService bookService;
 
-    @GetMapping(value = "/books")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping(value = "/books1")
     public List<Book> getCities() {
 
         List<Book> cities = bookService.findAll();
