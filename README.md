977d9f79c2b10314f6e98c43c070ed219d8cc6a9
tniu <tniu@infomedia.com.au>
2020-07-03 21:28:13 +1000

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# spring-examples
+docker build -t zengemily79/bookstore .
+docker image rm -f zengemily79/bookstore
+
+docker push zengemily79/bookstore:1.0.0
+docker run -p 8082:8082 zengemily79/bookstore
+
+http://localhost:8082/books
+
+docker exec -t -i [container ID] sh
+
+
+/logs/logs # cat access_log.2020-06-07.log 
+172.17.0.1 - - [07/Jun/2020:06:06:49 +0000] "GET /books1 HTTP/1.1" 404 286
+172.17.0.1 - - [07/Jun/2020:06:06:59 +0000] "GET /books HTTP/1.1" 200 120
+
+/logs/logs/application.log
+
+# Container Log
+
+
--- bookStore/src/main/java/example/controller/BookController.java
+++ bookStore/src/main/java/example/controller/BookController.java
@@ -2,6 +2,7 @@ package example.controller;
 
 import example.model.Book;
 import example.service.IBookService;
+//import hello.Greeter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,10 @@ public class BookController {
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/books")
     public List<Book> getBooks() {
+//        Greeter greeter = new Greeter();
         logger.debug("Debug getBooks" + new Date().toString());
         logger.info("Info getBooks"  + new Date().toString());
+//        logger.info(greeter.sayHello());
         List<Book> cities = bookService.findAll();
 
         return cities;
